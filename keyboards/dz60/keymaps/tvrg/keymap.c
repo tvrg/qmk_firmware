c27a8514c115bd15eaa3624720700c0e0920d682
Thomas von Rosenberg <thomas@von-rosenberg.eu>
2021-04-14 14:24:20 +0200

    Delete whole words with left thumb

--- keyboards/dz60/keymaps/tvrg/keymap.c
+++ keyboards/dz60/keymaps/tvrg/keymap.c
@@ -6,7 +6,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
         MO(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT,
         KC_LSFT, KC_ESC,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, XXXXXXX,
-        KC_LCTL, MO(2),   KC_LALT,                   KC_SPC,  KC_LGUI, KC_SPC,           KC_RALT, XXXXXXX,  MO(2),  KC_RCTL
+        KC_LCTL, MO(2),   KC_LALT,                C(KC_BSPC), KC_LGUI, KC_SPC,           KC_RALT, XXXXXXX,  MO(2),  KC_RCTL
     ),
 
     LAYOUT_60_iso_split_rshift(
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_UP,   KC_END,  _______, _______, _______,
         _______, _______, _______, _______, KC_PGUP, _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_INS,  _______, _______, _______,
-        _______, _______, _______,                   KC_DEL,  _______, _______,                   _______, _______, _______, _______
+        _______, _______, _______,                 C(KC_DEL), _______, _______,                   _______, _______, _______, _______
     ),
 
     LAYOUT_60_iso_split_rshift(
